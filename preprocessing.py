0f533461bc7ee59ca615083b942847bd145dd1e1
gunter <krotslya2@gmail.com>
2018-05-09 08:11:41 +0800

    normalize X

--- preprocessing.py
+++ preprocessing.py
@@ -14,6 +14,12 @@ def preprocessing(data_path):
     X = d[:, 0:13].astype(float)
     Y = d[:, 13]
     Y[Y > 0] = 1 # shape: (:, 1)
+
+    # normalize X
+    # X[:, 0] = (X[:, 0] - X[:, 0].min()) / (X[:, 0].max() - X[:, 0].min())
+    X = (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
+    # print(X)
+
     # Y to be 2-class
     encoder = LabelEncoder()
     encoder.fit(Y)
